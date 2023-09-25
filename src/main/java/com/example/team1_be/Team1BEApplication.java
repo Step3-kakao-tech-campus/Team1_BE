bd15cb7e1611002c1b65e5c519dc209caebb2d65
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-09-25 16:29:31 +0900

    rename : SpringBootApplication 파일명 변경 Be -> BE

--- src/main/java/com/example/team1_be/Team1BEApplication.java
+++ src/main/java/com/example/team1_be/Team1BEApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Team1BeApplication {
+public class Team1BEApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Team1BeApplication.class, args);
+		SpringApplication.run(Team1BEApplication.class, args);
 	}
 
 }
