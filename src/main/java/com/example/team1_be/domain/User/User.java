f5bdd666f7c22cb2cd781086a771bdce97cf598c
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-09-26 14:22:53 +0900

    feat : Entity 화 적용

--- src/main/java/com/example/team1_be/domain/User/User.java
+++ src/main/java/com/example/team1_be/domain/User/User.java
@@ -1,18 +1,31 @@
-    package com.example.team1_be.domain.User;
+package com.example.team1_be.domain.User;
 
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@RequiredArgsConstructor
 public class User {
     private final int MIN_NAME_LENGHT = 2;
-    private final String name;
-    private final String phoneNumber;
+    @Id
+    @Column(name = "USER_ID")
+    private int id;
+    private String name;
+    private String phoneNumber;
 
-    public User(String name, String phoneNumber) {
+    public User(int id, String name, String phoneNumber) {
         if (name.length() < MIN_NAME_LENGHT)
             throw new IllegalArgumentException("이름은 2글자 미만일 수 없습니다.");
 
+        this.id = id;
         this.name = name;
         this.phoneNumber = phoneNumber;
     }
-
 
     public String getName() {
         return name;
