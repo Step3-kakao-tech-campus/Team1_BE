6540150de07c229a740e7698bf0e5f7d3626c700
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-09-26 13:58:46 +0900

    feat : 유저 정보 조회 기능 추가

--- src/main/java/com/example/team1_be/domain/User/User.java
+++ src/main/java/com/example/team1_be/domain/User/User.java
@@ -1,4 +1,20 @@
 package com.example.team1_be.domain.User;
 
 public class User {
+    private final String name;
+    private final String phoneNumber;
+
+    public User(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 }
