c2fe3e5e11b8728ea766ff53e9fddddef58f681d
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-09-26 13:14:29 +0900

    docs : TDD를 위한 기본 테스트 목록 설정

--- src/test/java/com/example/team1_be/domain/User/UserTest.java
+++ src/test/java/com/example/team1_be/domain/User/UserTest.java
@@ -1,7 +1,17 @@
 package com.example.team1_be.domain.User;
 
+import org.junit.jupiter.api.DisplayName;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
+    /**
+     * 현 주석에서 사용자와 유저는 통용된다.
+     *
+     * 유저의 전화번호는 '-' 기호를 포함한 13자리여야만 한다.
+     * 유저의 전화번호는 'ddd-dddd-dddd' 양식을 지켜야 한다.
+     * 유저의 이름길이는 2글자 미만이 될 수 없다.
+     * 유저의 정보(이름, 전화번호)를 조회할 수 있다.
+     * 유저클래스는 JPA에서 하나의 DB 테이블에 속해야 하므로 엔티티로 관리되어야 한다.
+     */
 }
