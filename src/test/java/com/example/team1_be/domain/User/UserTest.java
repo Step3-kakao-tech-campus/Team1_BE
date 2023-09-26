36ed047ca53b2c250592e1a0cb5d472c5dfaff57
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-09-26 13:59:00 +0900

    test : 유저 정보 조회 테스트 추가

--- src/test/java/com/example/team1_be/domain/User/UserTest.java
+++ src/test/java/com/example/team1_be/domain/User/UserTest.java
@@ -1,7 +1,22 @@
 package com.example.team1_be.domain.User;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
+    @DisplayName("유저의 정보(이름, 전화번호)를 조회할 수 있다.")
+    @CsvSource({"이재훈, 010-1234-4466",
+                "안한주, 010-3333-3333"})
+    @ParameterizedTest
+    void test1(String name, String phoneNumber) {
+        User user = new User(name, phoneNumber);
+        assertThat(user.getName()).isEqualTo(name);
+        assertThat(user.getPhoneNumber()).isEqualTo(phoneNumber);
+    }
 }
