88e54399377d0dcd6e44fccc4c6b9f322cf6ff8c
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-09-26 14:06:08 +0900

    refactor : 유저 이름 최소길이 변수화

--- src/main/java/com/example/team1_be/domain/User/User.java
+++ src/main/java/com/example/team1_be/domain/User/User.java
@@ -1,11 +1,12 @@
     package com.example.team1_be.domain.User;
 
 public class User {
+    private final int MIN_NAME_LENGHT = 2;
     private final String name;
     private final String phoneNumber;
 
     public User(String name, String phoneNumber) {
-        if (name.length() < 2)
+        if (name.length() < MIN_NAME_LENGHT)
             throw new IllegalArgumentException("이름은 2글자 미만일 수 없습니다.");
 
         this.name = name;
