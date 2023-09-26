66937738394971f7bb2c4c3a3fc3a8e4e6c73e0d
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-09-27 00:43:43 +0900

    test : 유저 저장

--- src/test/java/com/example/team1_be/domain/User/UserRepositoryTest.java
+++ src/test/java/com/example/team1_be/domain/User/UserRepositoryTest.java
@@ -17,3 +17,15 @@ class UserRepositoryTest {
                 .phoneNumber("010-5538-6818")
                 .build();
     }
+
+    @DisplayName("유저를 저장할 수 있어야 한다.")
+    @Test
+    void test2() {
+        User user = User.builder()
+                .id(1)
+                .name("이재훈")
+                .phoneNumber("010-5538-6818")
+                .build();
+
+        userRepository.save(user);
+    }
