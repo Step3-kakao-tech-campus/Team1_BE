22ff4629bd7f5047170c3c341c48acf11337897a
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-09-26 16:59:39 +0900

    test : 그룹 정보 조회 테스트 구현

--- src/test/java/com/example/team1_be/domain/Group/GroupTest.java
+++ src/test/java/com/example/team1_be/domain/Group/GroupTest.java
@@ -3,6 +3,7 @@ package com.example.team1_be.domain.Group;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GroupTest {
@@ -11,5 +12,15 @@ class GroupTest {
     void test1() {
         Group group = new Group(1, "이재훈","051-3233-3333","부산광역시 ");
         Group group = new Group(1, "이재훈","051-3233-3333","부산광역시");
+    }
+
+    @DisplayName("그룹의 정보들을 조회할 수 있다.")
+    @Test
+    void test2() {
+        Group group = new Group(1, "이재훈","051-3233-3333","부산광역시");
+        assertThat(group.getId()).isEqualTo(1);
+        assertThat(group.getName()).isEqualTo("이재훈");
+        assertThat(group.getTelNumber()).isEqualTo("051-3233-3333");
+        assertThat(group.getAddress()).isEqualTo("부산광역시");
     }
 }
