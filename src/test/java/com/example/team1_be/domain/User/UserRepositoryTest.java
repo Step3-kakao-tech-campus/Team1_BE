d7b8702e24b630126baf6e5f5f31e5189a71450c
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-09-27 00:42:27 +0900

    test : 유저 생성

---
+++ src/test/java/com/example/team1_be/domain/User/UserRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.example.team1_be.domain.User;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+    @DisplayName("유저를 생성할 수 있어야 한다.")
+    @Test
+    void test1() {
+        User.builder()
+                .id(1)
+                .name("이재훈")
+                .phoneNumber("010-5538-6818")
+                .build();
+    }
