123b71bfffb7f9fb773e6d8e452153aa889cc39e
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-09-27 17:56:53 +0900

    feat : 스케줄 엔티티 생성

---
+++ src/main/java/com/example/team1_be/domain/Schedule/Schedule.java
@@ -0,0 +1,30 @@
+package com.example.team1_be.domain.Schedule;
+
+import com.example.team1_be.domain.Group.Group;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@RequiredArgsConstructor
+@Getter
+@Table(name = "Schedule_tb")
+public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "schedule_id")
+    private int id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @NotNull
+    private Group group;
+
+    @Builder
+    public Schedule(int id, Group group) {
+        this.id = id;
+        this.group = group;
+    }
+}
