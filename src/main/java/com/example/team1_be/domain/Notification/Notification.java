7c63fa9d9051b7336f25f3f2b5f1821d53df4ff0
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-09-27 13:39:40 +0900

    feat : 알림 기초 엔티티 생성

---
+++ src/main/java/com/example/team1_be/domain/Notification/Notification.java
@@ -0,0 +1,46 @@
+package com.example.team1_be.domain.Notification;
+
+import com.example.team1_be.domain.User.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@RequiredArgsConstructor
+@Getter
+@Table(name = "Notification_tb")
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int id;
+
+    @Column(length = 200, nullable = false)
+    @Size(max = 200)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private NotificationType type;
+
+    @NotNull
+    private Boolean isRead;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @NotNull
+//    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder
+    public Notification(int id, String content, NotificationType type, Boolean isRead, User user) {
+        this.id = id;
+        this.content = content;
+        this.type = type;
+        this.isRead = isRead;
+        this.user = user;
+    }
+}
