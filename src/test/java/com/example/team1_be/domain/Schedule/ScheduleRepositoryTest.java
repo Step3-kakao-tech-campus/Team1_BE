cedaf0e0093c2f910887c4841f43490b48ae2fd9
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-09-27 18:05:32 +0900

    test : 스케줄 저장

--- src/test/java/com/example/team1_be/domain/Schedule/ScheduleRepositoryTest.java
+++ src/test/java/com/example/team1_be/domain/Schedule/ScheduleRepositoryTest.java
@@ -1,14 +1,21 @@
 package com.example.team1_be.domain.Schedule;
 
 import com.example.team1_be.domain.Group.Group;
+import com.example.team1_be.domain.Group.GroupRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class ScheduleRepositoryTest {
+    @Autowired
+    private GroupRepository groupRepository;
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+
     @DisplayName("스케줄을 생성할 수 있다.")
     @Test
     void test1() {
@@ -23,5 +30,23 @@ class ScheduleRepositoryTest {
                 .id(1)
                 .group(group)
                 .build();
+    }
+
+    @DisplayName("스케줄을 저장할 수 있다.")
+    @Test
+    void test2() {
+        Group group = Group.builder()
+                .id(1)
+                .name("맘스터치")
+                .phoneNumber("011-1111-1111")
+                .address("부산광역시")
+                .build();
+        groupRepository.save(group);
+
+        Schedule schedule = Schedule.builder()
+                .id(1)
+                .group(group)
+                .build();
+        scheduleRepository.save(schedule);
     }
 }
