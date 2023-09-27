e33c1e4f6d7d9c57bd2bc7bff5e1d68369fd7049
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-09-27 20:03:09 +0900

    test : 요일 정보 저장

--- src/test/java/com/example/team1_be/domain/Day/WeekdayRepositoryTest.java
+++ src/test/java/com/example/team1_be/domain/Day/WeekdayRepositoryTest.java
@@ -1,10 +1,14 @@
 package com.example.team1_be.domain.Day;
 
 import com.example.team1_be.domain.Group.Group;
+import com.example.team1_be.domain.Group.GroupRepository;
 import com.example.team1_be.domain.Schedule.Schedule;
+import com.example.team1_be.domain.Schedule.ScheduleRepository;
 import com.example.team1_be.domain.Week.Week;
+import com.example.team1_be.domain.Week.WeekRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDateTime;
@@ -13,6 +17,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class WeekdayRepositoryTest {
+    @Autowired
+    private GroupRepository groupRepository;
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+    @Autowired
+    private WeekRepository weekRepository;
+    @Autowired
+    private DayRepository dayRepository;
+
     @DisplayName("요일을 생성할 수 있다.")
     @Test
     void test1() {
@@ -38,5 +51,36 @@ class WeekdayRepositoryTest {
                 .weekday(Weekday.Monday)
                 .week(week)
                 .build();
+    }
+
+    @DisplayName("요일을 저장할 수 있다.")
+    @Test
+    void test2() {
+        Group group = Group.builder()
+                .id(1)
+                .name("맘스터치")
+                .phoneNumber("010-1111-1111")
+                .address("부산광역시")
+                .build();
+        groupRepository.save(group);
+
+        Schedule schedule = Schedule.builder()
+                .id(1)
+                .group(group)
+                .build();
+        scheduleRepository.save(schedule);
+
+        Week week = Week.builder()
+                .id(1)
+                .schedule(schedule)
+                .startTime(LocalDateTime.now())
+                .build();
+        weekRepository.save(week);
+
+        Day day = Day.builder()
+                .weekday(Weekday.Monday)
+                .week(week)
+                .build();
+        dayRepository.save(day);
     }
 }
