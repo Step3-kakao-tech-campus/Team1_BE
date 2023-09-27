e724692032a79c9539a2629a3b57941bd7874d01
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-09-27 14:26:40 +0900

    test : 데이터베이스 초기화 기능 추가

--- src/test/java/com/example/team1_be/domain/Notification/NotificationRepositoryTest.java
+++ src/test/java/com/example/team1_be/domain/Notification/NotificationRepositoryTest.java
@@ -2,11 +2,13 @@ package com.example.team1_be.domain.Notification;
 
 import com.example.team1_be.domain.User.User;
 import com.example.team1_be.domain.User.UserRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import javax.persistence.EntityManager;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -15,6 +17,20 @@ class NotificationRepositoryTest {
     private NotificationRepository notificationRepository;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private EntityManager em;
+
+    @AfterEach
+    public void resetRepository() {
+        em.clear();
+        notificationRepository.deleteAll();
+        userRepository.deleteAll();
+        em.createNativeQuery("ALTER TABLE User_tb ALTER COLUMN `user_id` RESTART WITH 1")
+                .executeUpdate();
+        em.createNativeQuery("ALTER TABLE Notification_tb ALTER COLUMN `notification_id` RESTART WITH 1")
+                .executeUpdate();
+        em.clear();
+    }
 
     @DisplayName("알림을 생성할 수 있다.")
     @Test
