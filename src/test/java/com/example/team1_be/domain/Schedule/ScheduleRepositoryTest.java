58e2407224cc4a55810e6f7dfc6a65f0cdbb76a0
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-09-27 18:06:33 +0900

    test : 테스트 이후 데이터베이스 초기화

--- src/test/java/com/example/team1_be/domain/Schedule/ScheduleRepositoryTest.java
+++ src/test/java/com/example/team1_be/domain/Schedule/ScheduleRepositoryTest.java
@@ -2,10 +2,13 @@ package com.example.team1_be.domain.Schedule;
 
 import com.example.team1_be.domain.Group.Group;
 import com.example.team1_be.domain.Group.GroupRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.persistence.EntityManager;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +18,23 @@ class ScheduleRepositoryTest {
     private GroupRepository groupRepository;
     @Autowired
     private ScheduleRepository scheduleRepository;
+    @Autowired
+    private EntityManager em;
+
+    @AfterEach
+    public void resetRepository() {
+        em.clear();
+
+        scheduleRepository.deleteAll();
+        em.createNativeQuery("ALTER TABLE Group_tb ALTER COLUMN `group_id` RESTART WITH 1")
+                .executeUpdate();
+
+        groupRepository.deleteAll();
+        em.createNativeQuery("ALTER TABLE Group_tb ALTER COLUMN `group_id` RESTART WITH 1")
+                .executeUpdate();
+
+        em.clear();
+    }
 
     @DisplayName("스케줄을 생성할 수 있다.")
     @Test
