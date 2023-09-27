1e0040ade91d324ca47582f84c5700ace62d4400
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-09-27 16:27:20 +0900

    test : 그룹 저장

--- src/test/java/com/example/team1_be/domain/Group/GroupRepositoryTest.java
+++ src/test/java/com/example/team1_be/domain/Group/GroupRepositoryTest.java
@@ -2,13 +2,15 @@ package com.example.team1_be.domain.Group;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class GroupRepositoryTest {
-
+    @Autowired
+    private GroupRepository groupRepository;
 
     @DisplayName("그룹을 생성할 수 있다.")
     @Test
@@ -19,5 +21,17 @@ class GroupRepositoryTest {
                 .phoneNumber("010-5538-6818")
                 .address("부산광역시")
                 .build();
+    }
+
+    @DisplayName("그룹을 저장할 수 있다.")
+    @Test
+    void test2() {
+        Group group = Group.builder()
+                .id(1)
+                .name("이재훈")
+                .phoneNumber("010-5538-6818")
+                .address("부산광역시")
+                .build();
+        groupRepository.save(group);
     }
 }
