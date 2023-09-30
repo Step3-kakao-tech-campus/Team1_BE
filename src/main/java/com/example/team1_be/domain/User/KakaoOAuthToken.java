9349c5d4ce6953467fcc0eb16742ed1169e1f8fe
hanju <ahj1903@naver.com>
2023-10-01 03:11:29 +0900

    refactor : 카카오 id값 받아오기
    처음부터 다시했음..

--- src/main/java/com/example/team1_be/domain/User/KakaoOAuthToken.java
+++ src/main/java/com/example/team1_be/domain/User/KakaoOAuthToken.java
@@ -3,10 +3,11 @@ package com.example.team1_be.domain.User;
 import lombok.Data;
 
 @Data
-public class OAuthToken {
+public class KakaoOAuthToken {
     private String access_token;
     private String token_type;
     private String refresh_token;
     private int expires_in;
+    private String scope;
     private int refresh_token_expires_in;
 }
