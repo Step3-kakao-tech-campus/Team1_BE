fad75c1383c27801b923aed8b1ca3607b37aebb2
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-09-30 11:19:49 +0900

    feat : 대타신청 리포지토리

---
+++ src/main/java/com/example/team1_be/domain/Substitute/SubstitueRepository.java
@@ -0,0 +1,6 @@
+package com.example.team1_be.domain.Substitute;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubstitueRepository extends JpaRepository<Substitute, Integer> {
+}
