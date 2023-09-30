8140c61b05d7ef5b7b5d2ff13926c2404e0201c9
hanju <ahj1903@naver.com>
2023-09-30 13:35:03 +0900

    refactor : 토큰 정보 토대로 카카오 ID값 받아오기

--- src/main/java/com/example/team1_be/domain/User/UserController.java
+++ src/main/java/com/example/team1_be/domain/User/UserController.java
@@ -1,20 +1,24 @@
 package com.example.team1_be.domain.User;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Controller
 public class UserController {
     @GetMapping("/login/kakao")//Data 리턴해주는 컨트롤러 함수
-    public @ResponseBody String kakaoCallback(String code) {
+    public @ResponseBody String kakaoCallback(String code) { // Data를 리턴해주는 컨트롤러 함수
 
         // POST 방식으로 key=value 데이터를 요청 (카카오쪽으로)
         // 이 때 필요한 라이브러리가 RestTemplate, 얘를 쓰면 http 요청을 편하게 할 수 있다.
@@ -43,6 +47,38 @@ public class UserController {
                 kakaoTokenRequest, // 요청할 때 보낼 데이터
                 String.class // 요청 시 반환되는 데이터 타입
         );
-        return "카카오 토큰 요청 완료 : 토큰 요청에 대한 응답 : "+response;
+
+        ObjectMapper om = new ObjectMapper();
+        OAuthToken oauthToken = null;
+        try {
+            oauthToken = om.readValue(response.getBody(),OAuthToken.class);
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("카카오 엑세스 토큰:"+oauthToken.getAccess_token());
+
+        RestTemplate rt2 = new RestTemplate();
+
+        // HTTP POST를 요청할 때 보내는 데이터(body)를 설명해주는 헤더도 만들어 같이 보내줘야 한다.
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.add("Authorization","Bearer "+oauthToken.getAccess_token());
+        headers2.add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // 요청하기 위해 헤더(Header)와 데이터(Body)를 합친다.
+        // kakaoTokenRequest는 데이터(Body)와 헤더(Header)를 Entity가 된다.
+        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest2 =
+                new HttpEntity<>(headers2);
+
+        // POST 방식으로 Http 요청한다. 그리고 response 변수의 응답 받는다.
+        ResponseEntity<String> response2 = rt2.exchange(
+                "https://kapi.kakao.com/v2/user/me", // https://{요청할 서버 주소}
+                HttpMethod.POST, // 요청할 방식
+                kakaoProfileRequest2, // 요청할 때 보낼 데이터
+                String.class // 요청 시 반환되는 데이터 타입
+        );
+        return response2.getBody();
     }
 }
