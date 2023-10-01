579caedd5faa9d47c6181ade9b379b0c9b5c9784
hanju <ahj1903@naver.com>
2023-10-01 15:39:58 +0900

    feat : 회원가입, 회원찾기

--- src/main/java/com/example/team1_be/domain/User/UserService.java
+++ src/main/java/com/example/team1_be/domain/User/UserService.java
@@ -4,26 +4,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+
 @Service
 public class UserService {
+    @Autowired
+    private EntityManager em;
 
     @Autowired
     private UserRepository userRepository;
 
     @Transactional(readOnly = true)
     public User findUser(Long kakaoId){
-        User user = userRepository.findByKakaoId(kakaoId).orElseGet(()->{
-            return new User();
-        });
+        User user = userRepository.findByKakaoId(kakaoId)
+                .orElseGet(()->{return null;});
         return user;
     }
 
     @Transactional
-    public void signIn(User user){
+    public void register(User user){
         int id = user.getId();
         Long kakaoId = user.getKakaoId();
         String name = user.getName();
         String phoneNumber = user.getPhoneNumber();
         userRepository.save(user);
+        System.out.println("전화번호 길이 : " + phoneNumber.length());
+        em.flush();
     }
 }
