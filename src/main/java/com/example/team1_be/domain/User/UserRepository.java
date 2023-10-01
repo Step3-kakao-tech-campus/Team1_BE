af33a073e42adf4035f6846b78a9324c108ae915
hanju <ahj1903@naver.com>
2023-10-01 14:18:48 +0900

    refactor : kakaoid 값으로 user 찾기
    로그인 시 kakaoid 값으로 기존 회원인지 확인

--- src/main/java/com/example/team1_be/domain/User/UserRepository.java
+++ src/main/java/com/example/team1_be/domain/User/UserRepository.java
@@ -2,5 +2,8 @@ package com.example.team1_be.domain.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByKakaoId(Long kakaoId);
 }
