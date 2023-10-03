3f93e33e36aa35cb874751653f3029ed1de589cb
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-10-03 15:53:14 +0900

    feat : CustomUserDetails
    기존 UserDetails에 도메인상의 User 정보를 연결

---
+++ src/main/java/com/example/team1_be/utils/security/auth/UserDetails/CustomUserDetails.java
@@ -0,0 +1,50 @@
+package com.example.team1_be.utils.security.auth.UserDetails;
+
+import com.example.team1_be.domain.User.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+    private User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
