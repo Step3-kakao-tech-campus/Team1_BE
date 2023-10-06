f34f0b9bf0756c8b200ec70abba638be7d7ee3de
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-10-06 15:41:57 +0900

    test : Member Schedule 조회

---
+++ src/test/java/com/example/team1_be/domain/Member/MemberRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.example.team1_be.domain.Member;
+
+import com.example.team1_be.BaseTest;
+import com.example.team1_be.domain.Apply.ApplyRepository;
+import com.example.team1_be.domain.Day.DayRepository;
+import com.example.team1_be.domain.Group.Group;
+import com.example.team1_be.domain.Group.GroupRepository;
+import com.example.team1_be.domain.Notification.NotificationRepository;
+import com.example.team1_be.domain.Schedule.ScheduleRepository;
+import com.example.team1_be.domain.Substitute.SubstituteRepository;
+import com.example.team1_be.domain.User.User;
+import com.example.team1_be.domain.User.UserRepository;
+import com.example.team1_be.domain.Week.WeekRepository;
+import com.example.team1_be.domain.Worktime.WorktimeRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManager;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemberRepositoryTest extends BaseTest {
+
+    public MemberRepositoryTest(UserRepository userRepository, GroupRepository groupRepository, MemberRepository memberRepository, NotificationRepository notificationRepository, DayRepository dayRepository, ApplyRepository applyRepository, WeekRepository weekRepository, WorktimeRepository worktimeRepository, ScheduleRepository scheduleRepository, SubstituteRepository substituteRepository, EntityManager em) {
+        super(userRepository, groupRepository, memberRepository, notificationRepository, dayRepository, applyRepository, weekRepository, worktimeRepository, scheduleRepository, substituteRepository, em);
+    }
+
+    @DisplayName("멤버 조회")
+    @Test
+    void test1() {
+        assertThat(memberRepository.findById(1L).orElse(null))
+                .isNotEqualTo(null);
+    }
+
+    @DisplayName("멤버 전체 조회")
+    @Test
+    void test2() throws JsonProcessingException {
+        List<Member> members = memberRepository.findAll();
+        ObjectMapper om = new ObjectMapper();
+
+        for(Member member:members) {
+            Group group = member.getGroup();
+            User user = member.getUser();
+            String result = om.writeValueAsString(member);
+            System.out.println(result);
+        }
+    }
+}
