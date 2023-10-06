2d0b54e4d111ae5fc1fb402294364aba6aa90f70
차지원 <wona825@naver.com>
2023-10-07 02:23:27 +0900

    feat: create MemberExceptionHandler
    이미 다른 그룹에 소속된 멤버인 유저일 때 발생하는 예외 처리 추가

---
+++ src/main/java/com/example/team1_be/domain/Member/exception/MemberExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.team1_be.domain.Member.exception;
+
+import com.example.team1_be.utils.ApiUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class MemberExceptionHandler {
+    @ExceptionHandler(MemberExistsException.class)
+    public ResponseEntity<ApiUtils.ApiResult> handleMemberExistsException(MemberExistsException e) {
+        return new ResponseEntity(ApiUtils.error("이미 그룹이 존재하는 멤버입니다.", HttpStatus.CONFLICT), HttpStatus.CONFLICT);
+    }
+}
