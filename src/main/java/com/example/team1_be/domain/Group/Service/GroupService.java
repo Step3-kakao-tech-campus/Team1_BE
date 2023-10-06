ee935f55ad9f6393500e904b4aeb602d46d43cac
차지원 <wona825@naver.com>
2023-10-07 02:14:41 +0900

    feat: create GroupService (그룹 생성 기능 추가)

--- src/main/java/com/example/team1_be/domain/Group/Service/GroupService.java
+++ src/main/java/com/example/team1_be/domain/Group/Service/GroupService.java
@@ -1,12 +1,17 @@
 package com.example.team1_be.domain.Group.Service;
 
 import com.example.team1_be.domain.Group.Group;
+import com.example.team1_be.domain.Group.GroupCreateRequest;
 import com.example.team1_be.domain.Group.GroupRepository;
-import com.example.team1_be.domain.Group.Invite.Invite;
 import com.example.team1_be.domain.Group.Invite.InviteRepository;
+import com.example.team1_be.domain.Member.Member;
+import com.example.team1_be.domain.Member.MemberRepository;
+import com.example.team1_be.domain.Member.exception.MemberExistsException;
+import com.example.team1_be.domain.User.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -14,11 +19,39 @@ import java.util.UUID;
 public class GroupService {
     private final InviteRepository inviteRepository;
     private final GroupRepository groupRepository;
+    private final MemberRepository memberRepository;
 
 
-    public void generateInviteCode(){
-         String code = UUID.randomUUID().toString();
+    public void generateInviteCode() {
+        String code = UUID.randomUUID().toString();
 
     }
 
+    public void create(User user, GroupCreateRequest groupCreateRequest) {
+        // 멤버 조회
+        Optional<Member> optionalMember = memberRepository.findByUser(user);
+
+        // 이미 다른 그룹의 멤버라면 예외 발생
+        if (optionalMember.isPresent()) {
+            throw new MemberExistsException();
+        }
+
+        // 그룹 생성
+        Group group = Group.builder()
+                .name(groupCreateRequest.getMarketName())
+                .phoneNumber(groupCreateRequest.getPhoneNumber())
+                .businessNumber(groupCreateRequest.getMarketNumber())
+                .address(groupCreateRequest.getMainAddress() + groupCreateRequest.getDetailAddress())
+                .build();
+
+        // 멤버 객체 생성
+        Member member = Member.builder()
+                .isAdmin(true)
+                .group(group)
+                .user(user)
+                .build();
+
+        groupRepository.save(group);
+        memberRepository.save(member);
+    }
 }
