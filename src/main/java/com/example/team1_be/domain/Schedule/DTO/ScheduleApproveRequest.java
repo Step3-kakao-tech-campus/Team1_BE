77ce5c246468c89c53c1f8f69bf7dda2485d98f5
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-10-12 03:10:59 +0900

    move, refactor : ScheduleApproveRequest파일 이동 및 개선

---
+++ src/main/java/com/example/team1_be/domain/Schedule/DTO/ScheduleApproveRequest.java
@@ -0,0 +1,15 @@
+package com.example.team1_be.domain.Schedule.DTO;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 스케줄 확정 Request DTO
+ */
+public class ScheduleApproveRequest {
+    @Getter
+    @NoArgsConstructor
+    private static class Request {
+        private Integer selection;  //선택한 추천 스케줄 id
+    }
+}
