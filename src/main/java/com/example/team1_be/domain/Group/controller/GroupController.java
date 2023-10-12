599cee8f332d68f61b42fad73302a308a5236d19
차지원 <wona825@naver.com>
2023-10-13 04:39:54 +0900

    feat: 그룹원 전체 조회 및 그룹원 삭제 controller

--- src/main/java/com/example/team1_be/domain/Group/controller/GroupController.java
+++ src/main/java/com/example/team1_be/domain/Group/controller/GroupController.java
@@ -2,15 +2,14 @@ package com.example.team1_be.domain.Group.controller;
 
 import com.example.team1_be.domain.Group.GroupCreateRequest;
 import com.example.team1_be.domain.Group.GroupCreateResponse;
+import com.example.team1_be.domain.Group.GroupMemberListResponse;
 import com.example.team1_be.domain.Group.Service.GroupService;
+import com.example.team1_be.utils.ApiUtils;
 import com.example.team1_be.utils.security.auth.UserDetails.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -25,5 +24,17 @@ public class GroupController {
     public ResponseEntity<GroupCreateResponse> create(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestBody @Valid GroupCreateRequest groupCreateRequest) {
         GroupCreateResponse groupCreateResponse = groupService.create(userDetails.getUser(), groupCreateRequest);
         return ResponseEntity.ok(groupCreateResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiUtils.ApiResult> findAll(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        GroupMemberListResponse groupMemberListResponse = groupService.findAll(userDetails.getUser());
+        return ResponseEntity.ok(ApiUtils.success(groupMemberListResponse));
+    }
+
+    @DeleteMapping("/member/id/{memberId}")
+    public ResponseEntity<?> deleteMember(@AuthenticationPrincipal CustomUserDetails userDetails, @PathVariable("memberId") Long memberId) {
+        groupService.deleteMember(userDetails.getUser(), memberId);
+        return ResponseEntity.ok(null);
     }
 }
