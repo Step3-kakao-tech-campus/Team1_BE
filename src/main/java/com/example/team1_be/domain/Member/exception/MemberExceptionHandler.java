4bb8821d5bc8699a99706aceb4561f9c26ad2ed5
차지원 <wona825@naver.com>
2023-10-13 04:15:27 +0900

    feat: 예외 추가에 따른 handler 추가

--- src/main/java/com/example/team1_be/domain/Member/exception/MemberExceptionHandler.java
+++ src/main/java/com/example/team1_be/domain/Member/exception/MemberExceptionHandler.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class MemberExceptionHandler {
     @ExceptionHandler(MemberExistsException.class)
     public ResponseEntity<ApiUtils.ApiResult> handleMemberExistsException(MemberExistsException e) {
-        return new ResponseEntity(ApiUtils.error("이미 그룹이 존재하는 멤버입니다.", HttpStatus.CONFLICT), HttpStatus.CONFLICT);
+        return new ResponseEntity<>(ApiUtils.error("이미 그룹이 존재하는 멤버입니다.", HttpStatus.CONFLICT), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    public ResponseEntity<ApiUtils.ApiResult> handleMemberNotFoundException(MemberNotFoundException e) {
+        return new ResponseEntity<>(ApiUtils.error(e.getMessage(), HttpStatus.NOT_FOUND), HttpStatus.NOT_FOUND);
     }
 }
