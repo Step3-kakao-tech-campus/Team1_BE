6b0d5a3eb181004382833ea53dd76be9fb616d82
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-10-13 23:07:53 +0900

    feat : 초대장 확인 컨트롤러 구현

--- src/main/java/com/example/team1_be/domain/Group/GroupController.java
+++ src/main/java/com/example/team1_be/domain/Group/GroupController.java
@@ -2,6 +2,8 @@ package com.example.team1_be.domain.Group;
 
 import com.example.team1_be.domain.Group.DTO.Create;
 import com.example.team1_be.domain.Group.DTO.InvitationAccept;
+import com.example.team1_be.domain.Group.Invite.DTO.InvitationCheck;
+import com.example.team1_be.domain.Group.Invite.InviteService;
 import com.example.team1_be.utils.ApiUtils;
 import com.example.team1_be.utils.security.auth.UserDetails.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
@@ -17,12 +19,21 @@ import javax.validation.Valid;
 public class GroupController {
 
     private final GroupService groupService;
+    private final InviteService inviteService;
 
     @PostMapping
     public ResponseEntity<?> create(@AuthenticationPrincipal CustomUserDetails userDetails,
                                     @RequestBody @Valid Create.Request request) {
         groupService.create(userDetails.getUser(), request);
         ApiUtils.ApiResult<String> response = ApiUtils.success(null);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/invitation/information/{invitationKey}")
+    public ResponseEntity<?> invitationCheck(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                             @PathVariable("invitationKey") String invitationKey) {
+        InvitationCheck.Response responseDTO = inviteService.invitationCheck(invitationKey);
+        ApiUtils.ApiResult<InvitationCheck.Response> response = ApiUtils.success(responseDTO);
         return ResponseEntity.ok(response);
     }
 
