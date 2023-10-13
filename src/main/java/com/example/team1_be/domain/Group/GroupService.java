4adce2de5862437551b3594b2e7516abac2a5653
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-10-13 15:31:07 +0900

    feat : create 서비스 구현

--- src/main/java/com/example/team1_be/domain/Group/GroupService.java
+++ src/main/java/com/example/team1_be/domain/Group/GroupService.java
@@ -1,19 +1,34 @@
 package com.example.team1_be.domain.Group;
 
 import com.example.team1_be.domain.Group.DTO.Create;
+import com.example.team1_be.domain.Member.Member;
+import com.example.team1_be.domain.Member.MemberRepository;
 import com.example.team1_be.domain.User.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.validation.Valid;
-
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class GroupService {
-    @Transactional
-    public void create(User user, Create.@Valid Request groupCreateRequest) {
+    private final GroupRepository groupRepository;
+    private final MemberRepository memberRepository;
 
+    @Transactional
+    public void create(User user, Create.Request request) {
+        Group group = Group.builder()
+                .name(request.getMarketName())
+                .address(request.getMainAddress()+request.getDetailAddress())
+                .businessNumber(request.getMarketNumber())
+                .build();
+        groupRepository.save(group);
+
+        Member member = Member.builder()
+                .user(user)
+                .isAdmin(true)
+                .group(group)
+                .build();
+        memberRepository.save(member);
     }
 }
