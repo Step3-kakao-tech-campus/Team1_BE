c7a2845b7a7b1775721a9df74d6e3fa8ceaf8320
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-10-13 15:30:06 +0900

    refactor : DTO validation 추가

--- src/main/java/com/example/team1_be/domain/Group/DTO/Create.java
+++ src/main/java/com/example/team1_be/domain/Group/DTO/Create.java
@@ -5,15 +5,21 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 public class Create {
     @Getter
     @AllArgsConstructor
     @NoArgsConstructor
     @Builder
     public static class Request {
+        @NotBlank(message = "매장이름이 누락되었습니다.")
         private String marketName;
+        @NotBlank(message = "매장번호가 누락되었습니다.")
         private String marketNumber;
+        @NotBlank(message = "도로명주소가 누락되었습니다.")
         private String mainAddress;
+        @NotBlank(message = "건물번호가 누락되었습니다.")
         private String detailAddress;
     }
 }
