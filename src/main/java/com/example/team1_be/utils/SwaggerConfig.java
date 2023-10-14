4e95798405e307f5bc562dab064759cc63810ada
hanju <ahj1903@naver.com>
2023-10-14 20:02:25 +0900

    feat : create swaggerconfig file

---
+++ src/main/java/com/example/team1_be/utils/SwaggerConfig.java
@@ -0,0 +1,12 @@
+package com.example.team1_be.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public 
+
+}
