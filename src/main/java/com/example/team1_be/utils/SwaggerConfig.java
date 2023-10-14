ccb530df0ced4168d95ea1a6d5bdd9acaf764630
hanju <ahj1903@naver.com>
2023-10-14 23:12:34 +0900

    feat : swagger Docket 설정

--- src/main/java/com/example/team1_be/utils/SwaggerConfig.java
+++ src/main/java/com/example/team1_be/utils/SwaggerConfig.java
@@ -2,11 +2,21 @@ package com.example.team1_be.utils;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public 
+    public Docket swagger(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.team1_be.domain"))
+                .paths(PathSelectors.any())
+                .build();
+    }
 
 }
