b0befab61d284d2ed828575d1aa469a1e4a43d61
eunjin917 <cgoya01@likelion.org>
2023-10-14 21:47:14 +0900

    test: 만료된 토큰에 대해 에러

--- src/test/java/com/example/team1_be/domain/User/UserControllerTest.java
+++ src/test/java/com/example/team1_be/domain/User/UserControllerTest.java
@@ -36,14 +36,17 @@ class UserControllerTest {
     private MockMvc mvc;
     @Autowired
     private ObjectMapper om;
-    private final String KAKAO_TOKEN = "U9fuJNXvhMvuuRY3ZhW2heGCfAk423eQc-su0hQGCj1zTgAAAYsVW77c";
+
+    private final String EXPIRED_KAKAO_TOKEN = "U9fuJNXvhMvuuRY3ZhW2heGCfAk423eQc-su0hQGCj1zTgAAAYsVW77c";  // 12시간 경과 후 만료된 토큰
+    // 토큰 새로 발급받고 변경 필수 : https://kauth.kakao.com/oauth/authorize?client_id=ba5bf7b3c440fb54f054ac5c3bfff761&redirect_uri=http://localhost:8080/login/kakao&response_type=code
+    private final String NEW_KAKAO_TOKEN = "U9fuJNXvhMvuuRY3ZhW2heGCfAk423eQc-su0hQGCj1zTgAAAYsVW77c";
 
     @DisplayName("토큰기반 회원가입 성공")
     @Test
     void test1() throws Exception {
         UserRequest.JoinDTO joinDTO = UserRequest.JoinDTO.builder()
                 .name("최은진")
-                .accessToken(KAKAO_TOKEN)
+                .accessToken(NEW_KAKAO_TOKEN)
                 .build();
         String content = om.writeValueAsString(joinDTO);
 
@@ -68,12 +71,27 @@ class UserControllerTest {
                 .isInstanceOf(NestedServletException.class);
     }
 
+    @DisplayName("토큰기반 회원가입 실패(토큰만료)")
+    @Test
+    void join_test_token_expiration() throws Exception {
+        UserRequest.JoinDTO joinDTO = UserRequest.JoinDTO.builder()
+                .name("최은진")
+                .accessToken(EXPIRED_KAKAO_TOKEN)
+                .build();
+        String content = om.writeValueAsString(joinDTO);
+
+        assertThatThrownBy(() -> mvc.perform(post("/auth/join")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content)))
+                .isInstanceOf(Exception404.class);
+    }
+
     @DisplayName("토큰기반 로그인 성공")
     @Test
     void test3() throws Exception {
         UserRequest.JoinDTO joinDTO = UserRequest.JoinDTO.builder()
                 .name("최은진")
-                .accessToken(KAKAO_TOKEN)
+                .accessToken(NEW_KAKAO_TOKEN)
                 .build();
         String joinDTOContent = om.writeValueAsString(joinDTO);
         mvc.perform(post("/auth/join")
@@ -81,7 +99,7 @@ class UserControllerTest {
                 .content(joinDTOContent));
 
         UserRequest.LoginDTO loginDTO = UserRequest.LoginDTO.builder()
-                .accessToken(KAKAO_TOKEN)
+                .accessToken(NEW_KAKAO_TOKEN)
                 .build();
         String loginDTOContent = om.writeValueAsString(loginDTO);
         ResultActions perform = mvc.perform(post("/auth/login")
@@ -96,7 +114,7 @@ class UserControllerTest {
     void test4() throws Exception {
         UserRequest.JoinDTO joinDTO = UserRequest.JoinDTO.builder()
                 .name("최은진")
-                .accessToken(KAKAO_TOKEN)
+                .accessToken(NEW_KAKAO_TOKEN)
                 .build();
         String joinDTOContent = om.writeValueAsString(joinDTO);
         mvc.perform(post("/auth/join")
@@ -110,5 +128,27 @@ class UserControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(loginDTOContent)))
                 .isInstanceOf(NestedServletException.class);
+    }
+
+    @DisplayName("토큰기반 로그인 실패(토큰만료)")
+    @Test
+    void login_test_token_expiration() throws Exception {
+        UserRequest.JoinDTO joinDTO = UserRequest.JoinDTO.builder()
+                .name("최은진")
+                .accessToken(NEW_KAKAO_TOKEN)
+                .build();
+        String joinDTOContent = om.writeValueAsString(joinDTO);
+        mvc.perform(post("/auth/join")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(joinDTOContent));
+
+        UserRequest.LoginDTO loginDTO = UserRequest.LoginDTO.builder()
+                .accessToken(EXPIRED_KAKAO_TOKEN)
+                .build();
+        String loginDTOContent = om.writeValueAsString(loginDTO);
+        assertThatThrownBy(() -> mvc.perform(post("/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(loginDTOContent)))
+                .isInstanceOf(Exception404.class);
     }
 }
