eb5e5ad199b327db060d144c7f9cff2d455d7196
eunjin917 <cgoya01@likelion.org>
2023-10-16 01:27:11 +0900

    feat: Login  Request DTO -> code

---
+++ src/main/java/com/example/team1_be/domain/User/DTO/Login.java
@@ -0,0 +1,21 @@
+package com.example.team1_be.domain.User.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+public class Login {
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Request {
+        @NotBlank(message = "code가 누락되었습니다.")
+        private String code;
+    }
+
+}
--- src/main/java/com/example/team1_be/domain/User/UserController.java
+++ src/main/java/com/example/team1_be/domain/User/UserController.java
@@ -1,5 +1,6 @@
 package com.example.team1_be.domain.User;
 
+import com.example.team1_be.domain.User.DTO.Login;
 import com.example.team1_be.utils.security.auth.UserDetails.CustomUserDetails;
 import com.example.team1_be.utils.security.auth.kakao.KakaoOAuth;
 import com.example.team1_be.utils.security.auth.kakao.KakaoOAuthToken;
@@ -11,7 +12,10 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
 
 
 @Controller
@@ -21,8 +25,8 @@ public class UserController {
     private final KakaoOAuth kakaoOAuth;
 
     @GetMapping("/login/kakao")
-    public @ResponseBody ResponseEntity<String> kakaoCallback(String code) throws JsonProcessingException {
-        KakaoOAuthToken kakaoOAuthToken = kakaoOAuth.getToken(code);
+    public @ResponseBody ResponseEntity<String> kakaoCallback(@RequestBody @Valid Login.Request request) throws JsonProcessingException {
+        KakaoOAuthToken kakaoOAuthToken = kakaoOAuth.getToken(request.getCode());
         KakaoUserProfile kakaoOAuthProfile = kakaoOAuth.getProfile(kakaoOAuthToken);
 
         String login = userService.login(kakaoOAuthProfile);
