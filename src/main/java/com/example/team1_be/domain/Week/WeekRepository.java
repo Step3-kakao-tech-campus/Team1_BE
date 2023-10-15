230f4b9d71bbf6693c6b8deb192867ece680e0ed
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-10-15 20:36:56 +0900

    feat : find week by schedule, startdate, status

--- src/main/java/com/example/team1_be/domain/Week/WeekRepository.java
+++ src/main/java/com/example/team1_be/domain/Week/WeekRepository.java
@@ -1,7 +1,18 @@
 package com.example.team1_be.domain.Week;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 public interface WeekRepository extends JpaRepository<Week, Long> {
 
+    @Query("select w " +
+            "from Week w " +
+            "where w.schedule.id = :scheduleId" +
+            " and w.startDate = :startDate " +
+            "and w.status = :status")
+    Optional<Week> findByScheduleIdStartDateAndAndStatus(@Param("scheduleId") Long scheduleId, @Param("startDate")LocalDate startDate, @Param("status") WeekRecruitmentStatus status);
 }
