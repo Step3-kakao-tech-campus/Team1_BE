b92351e0fc424c2d552723a7d676a40c3b788370
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-10-21 15:29:07 +0900

    test : 스케줄과 특정일 기반 worktime 조회

--- src/test/java/com/example/team1_be/domain/Worktime/WorktimeRepositoryTest.java
+++ src/test/java/com/example/team1_be/domain/Worktime/WorktimeRepositoryTest.java
@@ -5,21 +5,29 @@ import com.example.team1_be.domain.Apply.Apply;
 import com.example.team1_be.domain.Apply.ApplyRepository;
 import com.example.team1_be.domain.Day.Day;
 import com.example.team1_be.domain.Day.DayRepository;
+import com.example.team1_be.domain.Group.Group;
 import com.example.team1_be.domain.Group.GroupRepository;
 import com.example.team1_be.domain.Member.MemberRepository;
 import com.example.team1_be.domain.Notification.NotificationRepository;
+import com.example.team1_be.domain.Schedule.Schedule;
 import com.example.team1_be.domain.Schedule.ScheduleRepository;
 import com.example.team1_be.domain.Substitute.SubstituteRepository;
+import com.example.team1_be.domain.User.User;
 import com.example.team1_be.domain.User.UserRepository;
 import com.example.team1_be.domain.Week.Week;
 import com.example.team1_be.domain.Week.WeekRepository;
+import com.example.team1_be.utils.errors.exception.NotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import javax.persistence.EntityManager;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class WorktimeRepositoryTest extends BaseTest {
@@ -55,5 +63,28 @@ class WorktimeRepositoryTest extends BaseTest {
         assertThat(apply).isNotEqualTo(null);
         Worktime worktime = apply.getWorktime();
         System.out.println(worktime.getId());
+    }
+
+    @DisplayName("일간 확정 근무 명단 조회")
+    @Test
+    void getDailyFixedApplies() {
+        // given
+        LocalDate selectedDate = LocalDate.parse("2023-10-09");
+
+        // when
+        User user = userRepository.findById(1L).orElse(null);
+        assertThat(user).isNotEqualTo(null);
+
+        Group group = groupRepository.findByUser(user.getId()).orElse(null);
+        assertThat(group).isNotEqualTo(null);
+
+        Schedule schedule = scheduleRepository.findByGroup(group).orElse(null);
+        assertThat(schedule).isNotEqualTo(null);
+
+        LocalDate date = selectedDate.minusDays(selectedDate.getDayOfWeek().getValue()-1);
+        int dayOfWeek = selectedDate.getDayOfWeek().getValue();
+        List<Worktime> worktimes = worktimeRepository.findBySpecificDateAndScheduleId(date, dayOfWeek, schedule.getId());
+        assertThat(worktimes.size()).isNotEqualTo(0);
+        System.out.println("worktime : " + worktimes.size());
     }
 }
