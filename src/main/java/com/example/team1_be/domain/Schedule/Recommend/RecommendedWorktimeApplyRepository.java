efc6830dd236141244cfe607cd5b53317d9ad5da
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-10-22 03:21:56 +0900

    feat : delete RecommendedWorktimeApply By weeklyScheduleIds

--- src/main/java/com/example/team1_be/domain/Schedule/Recommend/RecommendedWorktimeApplyRepository.java
+++ src/main/java/com/example/team1_be/domain/Schedule/Recommend/RecommendedWorktimeApplyRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface RecommendedWorktimeApplyRepository extends JpaRepository<RecommendedWorktimeApply, Long> {
 
+    @Query("delete from RecommendedWorktimeApply r where r.recommendedWeeklySchedule.id in :ids")
+    void deleteByWeeklyScheduleIds(@Param("ids") List<Long> ids);
 }
