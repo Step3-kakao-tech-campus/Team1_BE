bc107ab7f905d6451ac58039333a6acab3a93507
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-10-23 21:23:33 +0900

    feat: Week <-> Day 양방향 매핑

--- src/main/java/com/example/team1_be/domain/Week/Week.java
+++ src/main/java/com/example/team1_be/domain/Week/Week.java
@@ -1,5 +1,6 @@
 package com.example.team1_be.domain.Week;
 
+import com.example.team1_be.domain.Day.Day;
 import com.example.team1_be.domain.Schedule.Schedule;
 import com.example.team1_be.utils.audit.BaseEntity;
 import lombok.Builder;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @RequiredArgsConstructor
@@ -30,12 +32,16 @@ public class Week extends BaseEntity {
     @NotNull
     private Schedule schedule;
 
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "week")
+    private List<Day> day;
+
     @Builder
-    public Week(Long id, WeekRecruitmentStatus status, LocalDate startDate, Schedule schedule) {
+    public Week(Long id, WeekRecruitmentStatus status, LocalDate startDate, Schedule schedule, List<Day> day) {
         this.id = id;
         this.status = status;
         this.startDate = startDate;
         this.schedule = schedule;
+        this.day = day;
     }
 
     public Week updateStatus(WeekRecruitmentStatus status) {
