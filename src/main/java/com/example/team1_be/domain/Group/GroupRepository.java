16c979442703fce360db231cade779f8d3b86df7
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-10-25 22:10:37 +0900

    refactor: Member 의존성 제거를 위한 메서드 이전

--- src/main/java/com/example/team1_be/domain/Group/GroupRepository.java
+++ src/main/java/com/example/team1_be/domain/Group/GroupRepository.java
@@ -1,14 +1,6 @@
 package com.example.team1_be.domain.Group;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.Optional;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
-    @Query("select m.group " +
-            "from Member m " +
-            "where m.user.id = :userId")
-    Optional<Group> findByUser(@Param("userId") Long userId);
 }
