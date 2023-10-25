d1a023191094bf7fca77283a93b5ebb53813c891
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-10-25 22:20:08 +0900

    refactor: find group 메서드 변경

--- src/main/java/com/example/team1_be/domain/Group/Invite/InviteService.java
+++ src/main/java/com/example/team1_be/domain/Group/Invite/InviteService.java
@@ -3,9 +3,9 @@ package com.example.team1_be.domain.Group.Invite;
 import com.example.team1_be.domain.Group.DTO.GetInvitation;
 import com.example.team1_be.domain.Group.Group;
 import com.example.team1_be.domain.Group.Invite.DTO.InvitationCheck;
-import com.example.team1_be.domain.Member.Member;
 import com.example.team1_be.domain.Member.MemberService;
 import com.example.team1_be.domain.User.User;
+import com.example.team1_be.domain.User.UserService;
 import com.example.team1_be.utils.errors.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,6 +19,7 @@ import java.util.UUID;
 @Service
 @Transactional(readOnly = true)
 public class InviteService {
+    private final UserService userService;
     private final InviteRepository inviteRepository;
     private final MemberService memberService;
 
@@ -50,11 +51,10 @@ public class InviteService {
 
     @Transactional
     public GetInvitation.Response getInvitation(User user) {
-        Member member = memberService.findByUser(user);
         if (!user.getIsAdmin()) {
             throw new CustomException("매니저 계정만 초대장을 발급할 수 있습니다.", HttpStatus.FORBIDDEN);
         }
-        Group group = member.getGroup();
+        Group group = userService.findGroupByUser(user);
         Invite invite = findByGroup(group);
         renewInvitation(invite);
         return new GetInvitation.Response(invite.getCode());
