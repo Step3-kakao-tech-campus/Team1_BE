ae0613feebd02257f785b3de18d2c0e22b0fdeac
hanju <ahj1903@naver.com>
2023-11-01 23:48:40 +0900

    .

--- src/main/java/com/example/team1_be/domain/User/UnfinishedUser/UnfinishedUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.team1_be.domain.User.UnfinishedUser;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UnfinishedUserRepository extends JpaRepository<UnfinishedUser, Long> {
-	Optional<UnfinishedUser> findByCode(String code);
-}
