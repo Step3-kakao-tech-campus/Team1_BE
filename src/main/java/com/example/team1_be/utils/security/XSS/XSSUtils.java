ebe448b1257c54d7e6b286211e1b15a6dd9fbb8e
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-11-05 02:41:29 +0900

    refactor($Security): 삼항연산자 제거
    외부 로직에서 null 체크가 이루어지므로 중복 제거

--- src/main/java/com/example/team1_be/utils/security/XSS/XSSUtils.java
+++ src/main/java/com/example/team1_be/utils/security/XSS/XSSUtils.java
@@ -4,6 +4,6 @@ import org.apache.commons.text.StringEscapeUtils;
 
 public class XSSUtils {
 	public static String charEscape(String value) {
-		return value == null ? value : StringEscapeUtils.escapeHtml4(value);
+		return StringEscapeUtils.escapeHtml4(value);
 	}
 }
