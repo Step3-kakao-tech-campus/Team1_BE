69a432d37792974e0be7748c320ead64cb192790
eunjin917 <cgoya01@likelion.org>
2023-11-11 01:35:52 +0900

    feat: -21000 유효인증

--- src/main/java/com/example/team1_be/utils/security/auth/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/example/team1_be/utils/security/auth/CustomAuthenticationEntryPoint.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.example.team1_be.utils.errors.ClientErrorCode;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -21,7 +22,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 		AuthenticationException authException) throws IOException, ServletException {
-		ApiUtils.ApiError apiError = new ApiUtils.ApiError("유효하지 않은 인증입니다.", -21000);
+		ApiUtils.ApiError apiError = new ApiUtils.ApiError("유효하지 않은 인증입니다.", ClientErrorCode.NO_PERMISSION.getValue());
 		ApiUtils.ApiResult apiResult = new ApiUtils.ApiResult(false, null, apiError);
 
 		response.setCharacterEncoding("UTF-8");
