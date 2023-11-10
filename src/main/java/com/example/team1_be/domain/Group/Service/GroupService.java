cd92ad252d448c34763c1ba105c9e99cd97c49e7
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-11-10 17:08:42 +0900

    feat($Group): Group 서비스에 logger 추가

--- src/main/java/com/example/team1_be/domain/Group/Service/GroupService.java
+++ src/main/java/com/example/team1_be/domain/Group/Service/GroupService.java
@@ -2,6 +2,8 @@ package com.example.team1_be.domain.Group.Service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,26 +26,33 @@ public class GroupService {
 	private final InviteService inviteService;
 	private final GroupReadOnlyRepositoryService readOnlyRepositoryService;
 	private final GroupWriteOnlyRepositoryService writeOnlyRepositoryService;
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	public void create(User user, Create.Request request) {
 		Group group = request.toGroup();
 		writeOnlyRepositoryService.creatGroup(group);
+		logger.info("그룹 생성 됨, id  : {}", group.getId());
 
 		inviteService.createInviteWithGroup(group);
+		logger.info("초대장 생성");
 
 		userService.updateGroup(user, group);
+		logger.info("그룹 가입함");
 	}
 
 	public void invitationAccept(User user, InvitationAccept.Request request) {
 		Invite invite = inviteService.findInvitation(request.getInvitationKey());
+		logger.info("초대장 조회");
 
 		Group group = invite.getGroup();
 		userService.updateGroup(user, group);
+		logger.info("그룹 가입");
 	}
 
 	public GetMembers.Response getMembers(User user) {
 		Group group = userService.findGroupByUser(user);
 		List<User> users = group.getUsers();
+		logger.info("그룹원 조회");
 
 		return new GetMembers.Response(group, user, users);
 	}
--- src/main/java/com/example/team1_be/domain/Group/Service/GroupWriteOnlyRepositoryService.java
+++ src/main/java/com/example/team1_be/domain/Group/Service/GroupWriteOnlyRepositoryService.java
@@ -1,5 +1,7 @@
 package com.example.team1_be.domain.Group.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,8 +15,10 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 public class GroupWriteOnlyRepositoryService {
 	private final GroupRepository repository;
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	public void creatGroup(Group group) {
+		logger.info("그룹 저장됨");
 		repository.save(group);
 	}
 }
