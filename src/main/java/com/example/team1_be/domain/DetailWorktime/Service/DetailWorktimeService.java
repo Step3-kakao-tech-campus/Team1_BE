2ac72af15e1e4a2afd1c1d2c3c5ced446ebdcd6c
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-11-10 17:52:10 +0900

    refactor($DetailWorktime): 로깅 및 리펙토링

--- src/main/java/com/example/team1_be/domain/DetailWorktime/Service/DetailWorktimeService.java
+++ src/main/java/com/example/team1_be/domain/DetailWorktime/Service/DetailWorktimeService.java
@@ -4,6 +4,7 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +12,8 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,13 +33,15 @@ import lombok.RequiredArgsConstructor;
 public class DetailWorktimeService {
 	private final DetailWorktimeReadOnlyService readOnlyService;
 	private final DetailWorktimeWriteOnlyService writeOnlyService;
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	public void createDays(List<DetailWorktime> days) {
 		writeOnlyService.createDays(days);
+		logger.info("{}개의 상세 근무 시간이 생성되었습니다.", days.size());
 	}
 
-	public List<DetailWorktime> createDays(LocalDate week, List<Worktime> weeklyWorktimes, List<List<Long>> amount) {
-		return writeOnlyService.createDaysWithWorktimesAndAmounts(week, weeklyWorktimes, amount);
+	public void createDays(LocalDate week, List<Worktime> weeklyWorktimes, List<List<Long>> amount) {
+		writeOnlyService.createDaysWithWorktimesAndAmounts(week, weeklyWorktimes, amount);
 	}
 
 	public Map<String, List<Map<Worktime, List<Apply>>>> findAppliesByWorktimeAndDayAndStatus(List<Worktime> worktimes,
@@ -45,14 +50,21 @@ public class DetailWorktimeService {
 		for (DayOfWeek day : DayOfWeek.values()) {
 			List<Map<Worktime, List<Apply>>> dailyApplies = new ArrayList<>();
 			for (Worktime worktime : worktimes) {
-				Map<Worktime, List<Apply>> appliesByWorktime = new HashMap<>();
-				List<Apply> applies = readOnlyService.findAppliesByWorktimeAndStatus(worktime, status, day);
-				appliesByWorktime.put(worktime, applies);
-				dailyApplies.add(appliesByWorktime);
+				dailyApplies.add(findAppliesByWorktimeAndStatusForDay(worktime, status, day));
 			}
 			weeklyApplies.put(day.toString(), dailyApplies);
 		}
+		logger.info("일주일 동안의 신청 정보를 조회하였습니다.");
 		return weeklyApplies;
+	}
+
+	private Map<Worktime, List<Apply>> findAppliesByWorktimeAndStatusForDay(Worktime worktime, ApplyStatus status,
+		DayOfWeek day) {
+		List<Apply> applies = readOnlyService.findAppliesByWorktimeAndStatus(worktime, status, day);
+		logger.info("근무 시간 ID: {}, 요일: {}에 따른 신청 정보를 조회하였습니다.", worktime.getId(), day);
+		Map<Worktime, List<Apply>> appliesByWorktime = new HashMap<>();
+		appliesByWorktime.put(worktime, applies);
+		return appliesByWorktime;
 	}
 
 	public SortedMap<LocalDate, List<DetailWorktime>> findEndedByGroupAndYearMonth(Group group, YearMonth yearMonth) {
@@ -60,7 +72,7 @@ public class DetailWorktimeService {
 		LocalDate end = yearMonth.atEndOfMonth();
 
 		SortedMap<LocalDate, List<DetailWorktime>> monthlyDetailsWorktimesMap = new TreeMap<>(
-			(s1, s2) -> s1.compareTo(s2));
+			Comparator.naturalOrder());
 		for (LocalDate date = start; !date.isAfter(end); date = date.plusDays(1)) {
 			List<DetailWorktime> detailWorktimes = readOnlyService.findByGroupAndDateAndStatus(group, date,
 				WeekRecruitmentStatus.ENDED);
@@ -73,8 +85,10 @@ public class DetailWorktimeService {
 		}
 
 		if (monthlyDetailsWorktimesMap.isEmpty()) {
+			logger.warn("확정된 스케줄이 없습니다.");
 			throw new NotFoundException("확정된 스케줄이 없습니다.");
 		}
+		logger.info("그룹 ID: {}, 년월: {}에 따른 종료된 상세 근무 시간 정보를 조회하였습니다.", group.getId(), yearMonth);
 		return monthlyDetailsWorktimesMap;
 	}
 
--- src/main/java/com/example/team1_be/domain/DetailWorktime/Service/DetailWorktimeWriteOnlyService.java
+++ src/main/java/com/example/team1_be/domain/DetailWorktime/Service/DetailWorktimeWriteOnlyService.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,12 +21,14 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 public class DetailWorktimeWriteOnlyService {
 	private final DetailWorktimeRepository repository;
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	public List<DetailWorktime> createDays(List<DetailWorktime> days) {
-		return repository.saveAll(days);
+	public void createDays(List<DetailWorktime> days) {
+		logger.info("{}개의 상세 근무 시간 정보를 생성합니다.", days.size());
+		repository.saveAll(days);
 	}
 
-	public List<DetailWorktime> createDaysWithWorktimesAndAmounts(LocalDate startDate, List<Worktime> weeklyWorktimes,
+	public void createDaysWithWorktimesAndAmounts(LocalDate startDate, List<Worktime> weeklyWorktimes,
 		List<List<Long>> weeklyDemands) {
 		List<List<DetailWorktime>> weeklyDetailWorktimes = new ArrayList<>();
 		for (DayOfWeek day : DayOfWeek.values()) {
@@ -33,7 +37,8 @@ public class DetailWorktimeWriteOnlyService {
 			weeklyDetailWorktimes.add(createDailyDemands(date, weeklyWorktimes, weeklyDemands.get(weekdayOrdinal)));
 		}
 		List<DetailWorktime> falttenedDetailsWorktimes = flattenListOfListsImperatively(weeklyDetailWorktimes);
-		return createDays(falttenedDetailsWorktimes);
+		logger.info("주간 근무 시간 정보를 생성합니다.");
+		createDays(falttenedDetailsWorktimes);
 	}
 
 	private List<DetailWorktime> createDailyDemands(LocalDate date, List<Worktime> weeklyWorktimes,
@@ -42,10 +47,13 @@ public class DetailWorktimeWriteOnlyService {
 		for (int i = 0; i < weeklyWorktimes.size(); i++) {
 			dailyDetailWorktimes.add(createDayWithWorktimeAndAmount(date, weeklyWorktimes.get(i), dailyDemands.get(i)));
 		}
+		logger.info("{}에 대한 일간 근무 시간 정보를 생성합니다.", date);
 		return dailyDetailWorktimes;
 	}
 
 	private DetailWorktime createDayWithWorktimeAndAmount(LocalDate date, Worktime worktime, Long dailyDemand) {
+		logger.info("근무 시간 ID: {}, 요일: {}, 요청량: {}에 대한 상세 근무 시간 정보를 생성합니다.", worktime.getId(), date.getDayOfWeek(),
+			dailyDemand);
 		return DetailWorktime.builder()
 			.date(date)
 			.dayOfWeek(date.getDayOfWeek())
