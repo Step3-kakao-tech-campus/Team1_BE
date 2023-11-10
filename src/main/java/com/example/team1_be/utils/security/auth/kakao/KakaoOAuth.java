d757270edd4df8a0f01c1a49fe4f916e4a72e2aa
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-11-11 07:15:53 +0900

    logging

--- src/main/java/com/example/team1_be/utils/security/auth/kakao/KakaoOAuth.java
+++ src/main/java/com/example/team1_be/utils/security/auth/kakao/KakaoOAuth.java
@@ -1,5 +1,6 @@
 package com.example.team1_be.utils.security.auth.kakao;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -13,6 +14,7 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -20,6 +22,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class KakaoOAuth {
@@ -34,6 +37,7 @@ public class KakaoOAuth {
 	private String PROXY_PORT;
 
 	public KakaoOAuthToken getToken(String code) throws JsonProcessingException {
+		log.info("Getting Kakao OAuth token with code: {}", code);
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
@@ -43,37 +47,48 @@ public class KakaoOAuth {
 		params.add("redirect_uri", REDIRECT_URI);
 		params.add("code", code);
 
-		return executeRequest(
-			"https://kauth.kakao.com/oauth/token",
-			HttpMethod.POST,
-			headers,
-			params,
-			KakaoOAuthToken.class
+		KakaoOAuthToken token = executeRequest(
+				"https://kauth.kakao.com/oauth/token",
+				HttpMethod.POST,
+				headers,
+				params,
+				KakaoOAuthToken.class
 		);
+
+		log.info("Got Kakao OAuth token: {}", token);
+
+		return token;
 	}
 
 	public KakaoUserProfile getProfile(KakaoOAuthToken token) throws JsonProcessingException {
+		log.info("Getting Kakao user profile with token: {}", token.getAccess_token());
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Authorization", "Bearer " + token.getAccess_token());
 
-		return executeRequest(
-			"https://kapi.kakao.com/v2/user/me",
-			HttpMethod.POST,
-			headers,
-			null,
-			KakaoUserProfile.class
+		KakaoUserProfile userProfile = executeRequest(
+				"https://kapi.kakao.com/v2/user/me",
+				HttpMethod.POST,
+				headers,
+				null,
+				KakaoUserProfile.class
 		);
+
+		log.info("Got Kakao user profile: {}", userProfile);
+
+		return userProfile;
 	}
 
 	public <T> T executeRequest(String url, HttpMethod method, HttpHeaders headers, MultiValueMap<String, String> body,
-		Class<T> clazz) throws JsonProcessingException {
+								Class<T> clazz) throws JsonProcessingException {
+
+		log.info("Sending {} request to {}", method, url);
 
 		RestTemplate rt;
 		if (!isLocalMode()) {
 			HttpHost proxy = new HttpHost(PROXY_HOST_NAME, Integer.parseInt(PROXY_PORT));
 			CloseableHttpClient httpClient = HttpClients.custom()
-				.setProxy(proxy)
-				.build();
+					.setProxy(proxy)
+					.build();
 
 			HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
 			factory.setConnectionRequestTimeout(360000);
@@ -93,11 +108,26 @@ public class KakaoOAuth {
 			requestEntity = new HttpEntity<>(headers);
 		}
 
-		ResponseEntity<String> response = rt.exchange(url, method, requestEntity, String.class);
+		ResponseEntity<String> response = null;
+		try {
+			response = rt.exchange(url, method, requestEntity, String.class);
+		} catch (Exception e) {
+			log.error("Error while sending {} request to {}: {}", method, url, e.getMessage());
+		}
+
+		if (response == null) {
+			throw new RuntimeException("Failed to send request to " + url);
+		}
+
+		log.info("Received response: {}", response);
 
 		ObjectMapper om = new ObjectMapper();
 
-		return om.readValue(response.getBody(), clazz);
+		T result = om.readValue(response.getBody(), clazz);
+
+		log.info("Parsed response: {}", result);
+
+		return result;
 	}
 
 	private boolean isLocalMode() {
